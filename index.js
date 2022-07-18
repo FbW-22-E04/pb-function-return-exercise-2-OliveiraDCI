ce6e82a2a9e66973b1e14982dbd8ed18608856d7
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-07-18 14:12:23 +0200

    Exercise concluded.

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 // 1
 // Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
 function vowel_count(str1) {
-  //write your code here
+  return str1.replace(/[^a|e|i|o|u|y]/gi, "").length;
 }
 console.log(vowel_count("The quick brown fox"));
 
 //2
 // Write a JavaScript function that generates a string id (specified length) of random characters.
 function makeid(l) {
-  //write your code here
+  let charList =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let stringID = "";
+  for (let i = 1; i <= l; i++) {
+    stringID += charList.charAt(Math.floor(Math.random() * charList.length));
+  }
+  return stringID;
 }
 console.log(makeid(8));
 
 // 3
 // Write a JavaScript program to pass a 'JavaScript function' as parameter.
-function abc() {
-  //write your code here
+function abc(id, callBack) {
+  callBack();
 }
 
-abc();
+function anotherFunction() {
+  console.log("Hello");
+}
+
+abc(1, anotherFunction);
